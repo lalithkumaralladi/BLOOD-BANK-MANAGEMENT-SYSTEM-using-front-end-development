8fcc3180a9a5a509e8971471591d5f8c3d9d98a0
lalithkumaralladi <2410030508@klh.edu.in>
2025-11-04 22:50:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    },
+    chunkSizeWarningLimit: 1000
+  },
   server: {
     port: 3000,
     open: true
   }
-})
+});
