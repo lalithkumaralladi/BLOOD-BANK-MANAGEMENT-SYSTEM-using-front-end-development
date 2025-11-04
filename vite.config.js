4e42c743bc3891d2094f8a693147a67802d0b243
lalithkumaralladi <2410030508@klh.edu.in>
2025-11-04 22:53:32 +0530

    Refactor Vite config input path to use resolve

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  base: '/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './index.html'
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
     },
     chunkSizeWarningLimit: 1000
   },
