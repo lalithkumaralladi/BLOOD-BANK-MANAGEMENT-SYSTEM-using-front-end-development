f1c9f8416241b47b13e919a2dd79071239169304
A Lalith Kumar <2410030508@klh.edu.in>
2025-11-04 22:48:21 +0530

    Fix build config for Render

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: __dirname,
-  base: './',
+  root: '.',
+  base: '/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: resolve(__dirname, 'index.html')
-    }
+      input: './index.html'
+    },
+    chunkSizeWarningLimit: 1000
   },
   server: {
     port: 3000,
